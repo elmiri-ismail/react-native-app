f2cdbc9520c91681c76387dc24d55983b0516037
Miri Ismail <ismailmiri@gmail.com>
2022-02-28 14:59:43 +0100

    liaison de front-end avec back-end

--- myapp/components/home.js
+++ myapp/components/home.js
@@ -6,22 +6,45 @@ import {
     Text,
     Button, 
     TextInput,
+    Image,
+    Pressable,
     View,
     TouchableWithoutFeedback,
+    Alert,
   } from 'react-native';
 
-const Home=({route,navigation})=>{
+const Home=({route,navigation , logout})=>{
     const {name}=route.params;
     return(<TouchableWithoutFeedback>
-        <View  ><Text style={styles.wlc} >welcome {name} to your home page</Text>
-                <View style={styles.btn}><Button  onPress={()=>navigation.navigate("SignIn")} color='coral' title="log out"/></View>
+        <View  >
+        <Image
+        style={styles.img}
+        source={require('../assets/user.png')}
+      />
+
+          <Text style={styles.wlc} >welcome {name} to your home page</Text>
+          <View style={styles.btn}><Button  onPress={()=> Alert.alert(
+      "Thanks for using our app",
+      "Are you sure to logout?",
+      [
+        {
+          text: "logout",
+          onPress: () => navigation.navigate("SignUp"),
+          style: "cancel"
+        },{
+          text: "cancel",
+          onPress: () => console.log("cancel"),
+          style: "cancel"
+        },
+      ]
+    )} color='coral' title="log out"/></View>
 
         </View></TouchableWithoutFeedback>
     )
 }
 const styles=StyleSheet.create({
   wlc : {
-      marginTop : 100 ,
+      marginTop : 20 ,
       marginLeft : 40 ,
       fontSize: 20,
       fontWeight: "bold",
@@ -30,10 +53,16 @@ const styles=StyleSheet.create({
      },
      btn:{
       marginTop:30,
-      marginLeft:130,
-      marginRight:130,
+      marginLeft:150,
+      marginRight:150,
   },
-
+     img:{
+      marginTop:30,
+       width: 100,
+       height:100,
+       marginLeft:150,
+      marginRight:150,
+     }
 
 })
   
--- myapp/components/signin.js
+++ myapp/components/signin.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import Header2 from './header2';
 import {
   StyleSheet,
   Text,
